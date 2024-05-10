39e29bc744c7e1bb2703b95146c66b5acada5b83
엄혜인 <mickey_8688@naver.com>
2024-05-10 11:00:32 +0900

    Add files via upload

---
+++ team_introduction/clone_test/temp/main_script1.js
@@ -0,0 +1,11 @@
+function btnopen() {
+    
+    window.open('https://github.com/rodlsdyd');
+    
+    }
+    
+    function btnopen2() {
+    
+    window.open('https://blog.naver.com/rlslek3');
+    
+    }
